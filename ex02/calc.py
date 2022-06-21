adf0c24d245927bd1430ccd291d2dd0a11a00f14
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-06-21 17:53:25 +0900

    if __name__==__main__を追加しました #9

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,9 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-root=tk.Tk()
-root.title("tk")
-
 def button_click(event):
     btn=event.widget
     num=btn["text"]
@@ -16,21 +13,25 @@ def button_click(event):
     else:
         entry.insert(tk.END, num)
 
-entry=tk.Entry(root,
-                width=10,justify="right",
-                font=("Times New Roman",40))
-entry.grid(row=0,column=0,columnspan=3)
+if __name__=="__main__":
+    root=tk.Tk()
+    root.title("電卓")
+
+    entry=tk.Entry(root,
+                    width=10,justify="right",
+                    font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3)
     
-r=1
-c=0
-for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"=","+","-","*","/","**"]):
-    button=tk.Button(root,text=f"{num}",
-                    font=("Times New Roman", 30),
-                    width=4,height=2)
-    button.bind("<1>", button_click)
-    button.grid(row=r,column=c)
-    c+=1
-    if (i+1)%4==0:
-        r+=1
-        c=0
-root.mainloop()
+    r=1
+    c=0
+    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"=","+","-","*","/","**"]):
+        button=tk.Button(root,text=f"{num}",
+                        font=("Times New Roman", 30),
+                        width=4,height=2)
+        button.bind("<1>", button_click)
+        button.grid(row=r,column=c)
+        c+=1
+        if (i+1)%4==0:
+            r+=1
+            c=0
+    root.mainloop()
