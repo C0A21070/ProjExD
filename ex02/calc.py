8ed999263ca86f3f242ff57122ad00cea9d43eb6
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-06-21 16:46:24 +0900

    追加機能

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,7 +11,7 @@ def button_click(event):
         res=entry.get()
         ans=eval(res)
         entry.delete(0,tk.END)
-        entry.insert(tk.END,ans)
+        entry.insert(tk.END,ans) 
     #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
     else:
         entry.insert(tk.END, num)
@@ -23,14 +23,14 @@ entry.grid(row=0,column=0,columnspan=3)
     
 r=1
 c=0
-for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"=","+","-","*","/","**"]):
     button=tk.Button(root,text=f"{num}",
                     font=("Times New Roman", 30),
                     width=4,height=2)
     button.bind("<1>", button_click)
     button.grid(row=r,column=c)
     c+=1
-    if (i+1)%3==0:
+    if (i+1)%4==0:
         r+=1
         c=0
 root.mainloop()
