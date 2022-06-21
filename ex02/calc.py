f6e97348409559a09234d91c1f92ce6975ba49fa
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-06-21 15:51:31 +0900

    ６問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,6 +7,7 @@ root.title("tk")
 def button_click(event):
     btn=event.widget
     num=btn["text"]
+
     #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
     entry.insert(tk.END, num)
 
@@ -17,15 +18,14 @@ entry.grid(row=0,column=0,columnspan=3)
     
 r=1
 c=0
-n=[9,8,7,6,5,4,3,2,1,0]
-for num in n:
+for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+"]):
     button=tk.Button(root,text=f"{num}",
                     font=("Times New Roman", 30),
                     width=4,height=2)
     button.bind("<1>", button_click)
     button.grid(row=r,column=c)
     c+=1
-    if (num-1)%3==0:
+    if (i-1)%3==0:
         r+=1
         c=0
 
