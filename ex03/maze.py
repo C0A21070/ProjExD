0ed1def1830757c55b225786cc2df4597cd80f3b
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-06-28 15:23:34 +0900

    8,9,10問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -15,21 +16,30 @@ def main_proc():
     elif key=="Left":cx-=20
     elif key=="Right":cx+=20    
     canvas.coords("tori",cx,cy)
-    root.after(10,main_proc)
+    root.after(5,main_proc)
 
 if __name__=="__main__":
+    #タイトル
     root=tk.Tk()
     root.title("迷えるこうかとん")
-
-    canvas=tk.Canvas(root,height=1500,width=900,bg="black")
+    
+    #Canvasの生成
+    canvas=tk.Canvas(root,height=900,width=1500,bg="black")
     canvas.pack()
-
+    
+    #迷路の生成
+    maze_mk=mm.make_maze(15,9)
+    mm.show_maze(canvas,maze_mk)
+    
+    #こうかとんの描写
     tori=tk.PhotoImage(file="fig/5.png")
     cx,cy=300,400
     canvas.create_image(cx,cy,image=tori,tag="tori")
-
+    
+    #キー入力
     key=""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
-    root.after(10,main_proc)
+    root.after(5,main_proc)
+
     root.mainloop()
