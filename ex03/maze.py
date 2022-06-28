6d94a52804c68778c19259932d6f2c8d07f231fb
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-06-28 14:35:03 +0900

    4,5,6問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,14 @@
 from email.mime import image
 import tkinter as tk
+
+def key_down(event):
+    global key
+    key=event.keysym
+
+def key_up():
+    global key
+    key=""
+
 if __name__=="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん")
@@ -11,4 +20,7 @@ if __name__=="__main__":
     cx,cy=300,400
     canvas.create_image(cx,cy,image=tori,tag="tori")
 
+    key=""
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
     root.mainloop()
