3318a0e1ca38f6bea9ad85fdd62b263294612269
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-06-28 14:26:50 +0900

    2,3問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,14 @@
+from email.mime import image
 import tkinter as tk
 if __name__=="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん")
-    root.geometry("500x200")
+
+    canvas=tk.Canvas(root,height=1500,width=900,bg="black")
+    canvas.pack()
+
+    tori=tk.PhotoImage(file="fig/5.png")
+    cx,cy=300,400
+    canvas.create_image(cx,cy,image=tori,tag="tori")
+
     root.mainloop()
