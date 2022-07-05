8fc89f9f2b2aeca0cdee0b221b49eb371ae87a77
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-07-05 16:04:27 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -19,8 +19,9 @@ def main():
     kk_rct.center = 900, 400
     #爆弾
     bmimg_sfc=pg.Surface((20,100))          #Surface
+    bmimg_sfc.set_colorkey((0,0,0))
     pg.draw.circle(bmimg_sfc,(255, 0, 0), (10,10), 10)
-    bmimg_rct= bmimg_sfc.get_rect()         #Rect
+    bmimg_rct=bmimg_sfc.get_rect()         #Rect
     bmimg_rct.centerx = randint(0,screen_rct.width)
     bmimg_rct.centery = randint(0,screen_rct.height)
     vx,vy= +1,+1
@@ -36,14 +37,28 @@ def main():
         if key_states[pg.K_DOWN] == True : kk_rct.centery +=1     #Y座標を+1
         if key_states[pg.K_LEFT] == True : kk_rct.centerx -=1     #X座標を-1
         if key_states[pg.K_RIGHT] == True : kk_rct.centerx +=1    #X座標を+1
+        if check_bound(kk_rct,screen_rct) != (1,1):               #領域外だったら
+            if key_states[pg.K_UP] == True : kk_rct.centery -=1       #Y座標を-1
+            if key_states[pg.K_DOWN] == True : kk_rct.centery +=1     #Y座標を+1
+            if key_states[pg.K_LEFT] == True : kk_rct.centerx -=1     #X座標を-1
+            if key_states[pg.K_RIGHT] == True : kk_rct.centerx +=1    #X座標を+1
         screen_sfc.blit(kk_img,kk_rct)     #画像の表示
 
         bmimg_rct.move_ip(vx,vy)
         screen_sfc.blit(bmimg_sfc,bmimg_rct)        #爆弾の表示
+        yk,tt = check_bound(bmimg_rct,screen_rct)
+        vx *=yk
+        vy *=tt
+
+        if kk_rct.colliderect(bmimg_rct) : return
 
         pg.display.update()
         clock.tick(1000)        #1000fpsの時を刻む
-
+def check_bound(rct,scr_rct):
+    yoko,tate = +1 , +1
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right : yoko = -1
+    if rct.top < scr_rct.top or scr_rct.bottom  < rct.bottom : tate = -1
+    return (yoko,tate)
 if __name__=="__main__":
     pg.init()       #モジュールを初期化
     main()          #ゲームのメイン関数
