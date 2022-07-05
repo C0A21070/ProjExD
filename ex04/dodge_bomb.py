379c07a2d4ec1681c521a973523fa95406ba6b9e
hiroharu suzuki <c0a2107084@edu.teu.ac.jp>
2022-07-05 14:23:54 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,15 +6,19 @@ def main():
     
     pg.display.set_caption("逃げろ！こうかとん") #タイトルバー    
     screen_sfc=pg.display.set_mode((1600,900))      #1400*800の画面Surfaceを生成
-    #screen__rct=
 
     bg_img=pg.image.load("pg_bg.jpg")           #Surface
     bg_rect=bg_img.get_rect()                   #Rect
     screen_sfc.blit(bg_img,bg_rect) 
-    #pg.display.update()
+    
+    tori_img=pg.image.load("fig/6.png")       #Surface
+    tori_img=pg.transform.rotozoom(tori_img, 0, 2.0) #Surface
+    tori_rect=tori_img.get_rect()               #Rect
+    tori_rect.center= 900, 400
 
     while True:
-        screen_sfc.blit(bg_img,bg_rect)
+        screen_sfc.blit(bg_img,bg_rect) 
+        screen_sfc.blit(tori_img,tori_rect)
         for event in pg.event.get():        #イベントを繰り返して処理
             if event.type == pg.QUIT: return    #ウィンドウのXボタンをクリックしたら
         pg.display.update()
